6e96dbb50c67a6726c7c09431c2eb75270350d98
Govindarajan V <govindarajanv@gmail.com>
2021-04-09 12:37:02 +0000

    readthedocs

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import * as core from '@actions/core'
+import * as event from './event'
+import * as version from './version'
 
 async function run(): Promise<void> {
   try {
+
+    const tag = event.getCreatedTag()
+        if (tag && version.isSemVer(tag)) {
+        }
     core.setOutput('release-url','https://example.com')
   } catch (error) {
     core.setFailed(error.message)
